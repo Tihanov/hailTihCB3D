476c929c820ef75fc248ba5dc5faf996abeb706b
Nazariy <266444833552m@gmail.com>
2023-07-22 23:22:30 +0100

    refactor: remove deprecated weapon interface

--- Source/AndroidTest/Private/Inventory/InvWeaponItemInterface.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "..\..\Public\Inventory\InvWeaponItemInterface.h"
-
-
-// Add default functionality here for any IInHandItemInterface functions that are not pure virtual.
