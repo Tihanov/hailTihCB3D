f1cf332e973bc0b90cd5baba553ef6da5f5c7b9d
Nazariy <266444833552m@gmail.com>
2023-09-20 22:42:42 +0100

    feat: damage sense, after shooting NPC try to find actor

--- Source/AndroidTest/Private/Ai/Enemy/ANpcEnemyController.cpp
+++ Source/AndroidTest/Private/Ai/Enemy/ANpcEnemyController.cpp
@@ -5,6 +5,7 @@
 
 #include "Log.h"
 #include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISense_Damage.h"
 
 
 AANpcEnemyController::AANpcEnemyController()
@@ -23,11 +24,19 @@ void AANpcEnemyController::BeginPlay()
 		&AANpcEnemyController::ActorPerceptionInfoUpdatedCallback);
 }
 
+void AANpcEnemyController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	InPawn->OnTakePointDamage.AddDynamic(this, &AANpcEnemyController::PawnDamageCallback);
+}
+
 void AANpcEnemyController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 	ChasingActorCheck(DeltaTime);
+	//FocusCheck(DeltaTime);
 }
 
 AActor* AANpcEnemyController::GetCurrentChasingActor(bool& OutIsChasingActor) const
@@ -41,6 +50,23 @@ void AANpcEnemyController::ChaiseActor(AActor* NewActor)
 	const auto OldActor = CurrentChasingActor.Get();
 	CurrentChasingActor = NewActor;
 	OnChaseNewActorDelegate.Broadcast(NewActor, OldActor);
+	LastChasingActor = OldActor;
+}
+
+const AActor* AANpcEnemyController::GetLastChasingActor(bool& OutDoesExists) const
+{
+	OutDoesExists = LastChasingActor.IsValid();
+	return LastChasingActor.Get();
+}
+
+void AANpcEnemyController::FocusOnChasingActor()
+{
+	bIsFocusingNow = true;
+}
+
+void AANpcEnemyController::StopFocusingOnChasingActor()
+{
+	bIsFocusingNow = false;
 }
 
 void AANpcEnemyController::ChasingActorCheck(float DeltaTime)
@@ -54,9 +80,35 @@ void AANpcEnemyController::ChasingActorCheck(float DeltaTime)
 		ChaiseActor(nullptr);
 }
 
+void AANpcEnemyController::FocusCheck(float DeltaTime)
+{
+	if (!bIsFocusingNow)
+		return;
+	if(CurrentChasingActor.IsNull())
+	{
+		StopFocusingOnChasingActor();
+		return;
+	}
+
+	const auto TurnAngle = FMath::RadiansToDegrees(FMath::Acos(
+		FVector::DotProduct(-GetPawn()->GetActorForwardVector(), CurrentChasingActor->GetActorForwardVector())));
+
+	if(FMath::Abs(TurnAngle) > FMath::Abs(FocusTurnAngle))
+	{
+	}
+}
+
 void AANpcEnemyController::ActorPerceptionInfoUpdatedCallback(const FActorPerceptionUpdateInfo& UpdateInfo)
 {
 	if(CurrentChasingActor.IsNull() && UpdateInfo.Target.IsValid())
 		ChaiseActor(UpdateInfo.Target.Get());
 }
+
+void AANpcEnemyController::PawnDamageCallback(AActor* DamagedActor, float Damage, AController* InstigatedBy,
+	FVector HitLocation, UPrimitiveComponent* FHitComponent, FName BoneName, FVector ShotFromDirection,
+	const UDamageType* DamageType, AActor* DamageCauser)
+{
+	UAISense_Damage::ReportDamageEvent(GetWorld(), this, DamageCauser, Damage, HitLocation, HitLocation);
+}
+
 
--- Source/AndroidTest/Public/Ai/Enemy/ANpcEnemyController.h
+++ Source/AndroidTest/Public/Ai/Enemy/ANpcEnemyController.h
@@ -24,29 +24,52 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	virtual void OnPossess(APawn* InPawn) override;
+
 public:
 	virtual void Tick(float DeltaTime) override;
 	
 	UAIPerceptionComponent* GetEnemyPerceptionComponent() const { return EnemyPerceptionComponent; }
 	
-public: /*components:*/
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Components")
-		UAIPerceptionComponent* EnemyPerceptionComponent;
-
 	UFUNCTION(BlueprintPure, Category = "Ai")
 		AActor* GetCurrentChasingActor(UPARAM(DisplayName = "IsChasingActor?") bool& OutIsChasingActor) const;
 	UFUNCTION(BlueprintCallable, Category = "Ai")
 		void ChaiseActor(AActor* NewActor);
 
+	UFUNCTION(BlueprintPure, Category = "Ai")
+		const AActor* GetLastChasingActor(UPARAM(DisplayName = "OutDoesExists?") bool& OutDoesExists) const;
+
+	UFUNCTION(BlueprintCallable, Category = "Ai")
+		void FocusOnChasingActor();
+	UFUNCTION(BlueprintCallable, Category = "Ai")
+		void StopFocusingOnChasingActor();
+	UFUNCTION(BlueprintPure, Category = "Ai")
+		bool IsFocusingNow() const { return bIsFocusingNow; }
+	
+public: /*components:*/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Components")
+		UAIPerceptionComponent* EnemyPerceptionComponent;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Options")
+		float FocusTurnAngle = 45;
+	
 public: /*delegates:*/
 	UPROPERTY(BlueprintAssignable, Category = "Delegates", DisplayName = "OnChaseNewActor")
 		FOnChaseNewActorDelegate OnChaseNewActorDelegate;
 	
 private:
 	void ChasingActorCheck(float DeltaTime);
+	void FocusCheck(float DeltaTime);
 	UFUNCTION()
 		void ActorPerceptionInfoUpdatedCallback(const FActorPerceptionUpdateInfo& UpdateInfo);
-
+	UFUNCTION()
+		void PawnDamageCallback(AActor* DamagedActor, float Damage, class AController* InstigatedBy, FVector HitLocation,
+								class UPrimitiveComponent* FHitComponent, FName BoneName, FVector ShotFromDirection,
+								const class UDamageType* DamageType, AActor* DamageCauser);
+	
 private:
 	TSoftObjectPtr<AActor> CurrentChasingActor = nullptr;
+	TSoftObjectPtr<AActor> LastChasingActor = nullptr;
+	bool bIsFocusingNow = false;
 };
